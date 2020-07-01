cac1fe483c1b259e020d39dcd767e13c10983c5a
SIabo13 <raul0882@gmail.com>
2020-07-01 11:09:00 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include "includes/db.php"; ?>
+   <?php include "includes/header.php"; ?>
+
+   <?php include "includes/navigation.php"; ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+           <?php include "includes/blogentry.php";?>
+
+           <?php include "includes/sidebar.php"; ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+    <?php   include "includes/footer.php"; ?>
